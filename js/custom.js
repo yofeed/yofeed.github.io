726d628461e0eec34e5c3a16e571efb495bd5e3c
alexpmil <alexpmil@gmail.com>
2014-08-02 18:17:27 +0000

    update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 var title = $(".site-title").text();
 var map = { };
 var index = 1;
+var typing_speed = 125;
 for(i=1;i<title.length+1;i++){
     map[i] = title.substring(0,i)+"_";
     setTimeout(function(){
@@ -8,9 +9,9 @@ for(i=1;i<title.length+1;i++){
         console.log(map[index]);
         console.log(index);
         index += 1;
-    }, i*200);
+    }, i*typing_speed);
 }
 setTimeout(function(){
     $(".site-title").text($(".site-title").text().substring(0,title.length));
     $(".site-title").append('<blink>_</blink>');
-}, (title.length+1)*200)
+}, (title.length+1)*typing_speed)
