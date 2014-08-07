ec09e463b5fda48e5c70893e1c0ae1058268aae9
Alex Miller <alexpmil@gmail.com>
2014-08-07 13:51:07 -0400

    Create IEsucks.js

---
+++ js/IEsucks.js
@@ -0,0 +1,5 @@
+$('.blur-fill.back').foggy({
+   blurRadius: 7,          // In pixels.
+   opacity: 0.5,           // Falls back to a filter for IE.
+   cssFilterSupport: true  // Use "-webkit-filter" where available.
+ }); 
