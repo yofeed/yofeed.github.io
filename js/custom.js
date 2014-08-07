989c62f530a7456abfd3f46958504ab2e099e30f
Alex Miller <alexpmil@gmail.com>
2014-08-07 12:30:27 -0400

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,23 @@
-var title = $(".site-title").text();
-var map = { };
-var index = 1;
-var typing_speed = 125;
-for(i=1;i<title.length+1;i++){
-    map[i] = title.substring(0,i)+"_";
+$(document).ready(function(){
+    var title = $(".site-title").text();
+    var map = { };
+    var index = 1;
+    var typing_speed = 125;
+    for(i=1;i<title.length+1;i++){
+        map[i] = title.substring(0,i)+"_";
+        setTimeout(function(){
+            $(".site-title").text(map[index]);
+            console.log(map[index]);
+            console.log(index);
+            index += 1;
+        }, i*typing_speed);
+    }
     setTimeout(function(){
-        $(".site-title").text(map[index]);
-        console.log(map[index]);
-        console.log(index);
-        index += 1;
-    }, i*typing_speed);
-}
-setTimeout(function(){
-    $(".site-title").text($(".site-title").text().substring(0,title.length));
-    $(".site-title").append('<blink>_</blink>');
-}, (title.length+1)*typing_speed);
-$(".site-title").hover(
-    function(){$(this).addClass("glitch");},
-    function(){$(this).removeClass("glitch");}
-);
+        $(".site-title").text($(".site-title").text().substring(0,title.length));
+        $(".site-title").append('<blink>_</blink>');
+    }, (title.length+1)*typing_speed);
+    $(".site-title").hover(
+        function(){$(this).addClass("glitch");},
+        function(){$(this).removeClass("glitch");}
+    );
+});
