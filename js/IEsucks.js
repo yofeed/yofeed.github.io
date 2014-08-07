b6db2dd623d96432db0da1c22bfdf6301907959f
Alex Miller <alexpmil@gmail.com>
2014-08-07 13:59:29 -0400

    Update IEsucks.js

--- js/IEsucks.js
+++ js/IEsucks.js
@@ -1,5 +1,7 @@
+console.log("IE START");
 $('.blur-fill.back').foggy({
    blurRadius: 7,          // In pixels.
    opacity: 0.5,           // Falls back to a filter for IE.
    cssFilterSupport: true  // Use "-webkit-filter" where available.
  }); 
+ console.log("IE FINISHED");
