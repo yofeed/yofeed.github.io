acd149fa0cb7d90807839d98c6d01e12dfe5f2fa
Alex Miller <alexpmil@gmail.com>
2014-08-24 02:21:15 -0400

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -21,8 +21,7 @@ $(document).ready(function(){
     console.log('helloworld')
     $('.article-card').each(function(i, card){
         $($(card).find('.article-text')[0]).css('height', $(card).find('.article-img')[0].offsetHeight);
-        $($(card).find('.post-link')[0]).css('max-height', $(card).find('.article-img')[0].offsetHeight-17);
-        var H = $(card).find('.article-img')[0].offsetHeight - $(card).find('.provider-text')[0].offsetHeight - $(card).find('.post-date')[0].offsetHeight-5;
+        var H = $(card).find('.article-img')[0].offsetHeight - $(card).find('.provider-text')[0].offsetHeight - $(card).find('.post-date')[0].offsetHeight-10;
         var T = $(card).find('.post-link span')[0];
         if (T.offsetHeight > H) {
             console.log('fixing');
